649ce63d05db8b101438c4d64a56d06108dab8fc
timurmalkoc <39303404+timurmalkoc@users.noreply.github.com>
2020-03-14 16:34:49 -0400

    WebSide

--- git_MyProject/HelloWorld.java
+++ git_MyProject/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
         System.out.println("Hello guys!");
         System.out.println("Last Changes");
         System.out.println("Check if your local is ahead of origin master");
+	System.out.println("hehehe");
     }
 }
