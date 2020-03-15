de99ce671fee6a7c076c2780879ca832a14e150d
Timur <malkoctimurcelik@gmail.com>
2020-03-15 16:24:46 -0400

    our meesage

--- git_MyProject/Another.java
+++ git_MyProject/Another.java
@@ -4,7 +4,8 @@ public class Another {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("HEEEY");
+		System.out.println("HEEEY 2. try");
 	}
 
 }
